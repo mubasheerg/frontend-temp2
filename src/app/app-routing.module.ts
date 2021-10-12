f2a9fe40d5abf1262f45d45d4b1fc25f501d2a25
Mubasheer G <mubasheer.g@revature.com>
2021-10-12 22:46:35 +0530

    committed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './component/admin/admin-dashboard/admin-dashboard.component';
 import { CustomerDashboardComponent } from './component/customer/customer-dashboard/customer-dashboard.component';
 import { CustomerSignupComponent } from './component/customer/customer-signup/customer-signup.component';
+import { CustomerViewProductComponent } from './component/customer/customer-view-product/customer-view-product.component';
 import { LoginComponent } from './component/login/login.component';
 import { ViewAllOrderComponent } from './component/order/view-all-order/view-all-order.component';
 import { AddProductComponent } from './component/products/add-products/add-product.component';
@@ -19,6 +20,7 @@ const routes: Routes = [
   {path:'view-all-products',component:ViewAllProductComponent},
   {path:'view-all-orders',component:ViewAllOrderComponent}, 
   {path:'customer-dashboard',component:CustomerDashboardComponent},
+  {path:'customer-view-product',component:CustomerViewProductComponent},
   {path:'**',component:LoginComponent}
 
 ];
